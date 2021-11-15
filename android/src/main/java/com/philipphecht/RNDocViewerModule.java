42e9b12755ab31f0f2e74885d282f017603d4710
mike_meyer <mike.meyer@thebeecorp.com>
2021-11-15 08:18:50 -0500

    Changed android.support.v4
    Switched to androidx.core.content.FileProvider

--- android/src/main/java/com/philipphecht/RNDocViewerModule.java
+++ android/src/main/java/com/philipphecht/RNDocViewerModule.java
@@ -30,8 +30,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import android.support.v4.content.FileProvider;
-
+import android.support.v4.content.FileProvider;;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
